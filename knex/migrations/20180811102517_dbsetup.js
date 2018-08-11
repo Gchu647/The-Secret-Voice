cc327be3ed553d3a63952c4cd00903c3ac289712
Steven Inouye <steveninouye@msn.com>
2018-08-11 10:37:25 -1000

    seed & migration file template set up to be passed on to george

--- knex/migrations/20180811102517_dbsetup.js
+++ knex/migrations/20180811102517_dbsetup.js
@@ -1,8 +1,5 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', table => {});
 };
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = function(knex, Promise) {};
