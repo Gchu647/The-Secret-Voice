d9baf7c8487fcd0d73b12e1893162ed8061cb1a8
gchu <gchu647@gmail.com>
2018-08-11 15:21:18 -1000

    added audio_storage and edied content seed

--- db/migrations/20180811134657_content_table_setup.js
+++ db/migrations/20180811134657_content_table_setup.js
@@ -3,7 +3,8 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('contents', table => {
     table.increments();
     table.string('category').notNull();
-    table.string('audioLink').notNull();
+    table.string('title').notNull();
+    table.string('audio_link').notNull();
     table.integer('user_id').references('users.id');
     table.integer('likes').notNull().defaultTo(0); // updated column when liked
     table.timestamps(true, true);
