a2da726d419b777a89cc0d0abb7c2f63e590ae27
gchu <gchu647@gmail.com>
2018-08-11 14:27:03 -1000

    add in db_setup.sql

--- db/db_setup.sql
+++ db/db_setup.sql
@@ -4,6 +4,7 @@ DROP DATABASE IF EXISTS secret_voice;
 DROP USER IF EXISTS secret_user;
 
 CREATE USER secret_user WITH PASSWORD 'password';
+-- Dont log in with secret_user
 CREATE DATABASE secret_voice WITH OWNER secret_user;
 
 \c secret_voice secret_user
